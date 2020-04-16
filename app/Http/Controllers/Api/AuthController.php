ac665202093264a0baa90cb9a0071ef990ae99ac
Olaide Abdulazeez <dj3plles@gmail.com>
2020-04-16 10:28:15 +0100

    send registration mail via redis

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignUp;
+use App\Jobs\SendRegistrationMail;
 use App\Mail\UserRegistered;
 use App\User;
 use Illuminate\Http\Request;
@@ -18,12 +19,12 @@ class AuthController extends Controller
         $user_model = User::whereEmail($request->email);
 
         if (!$user_model->exists()) {
-            return $this->respondWithError(['Email address not found.']);
+            return $this->respondWithError('Email address not found.');
         }
 
         $user = $user_model->first();
         if (!Hash::check($request->password, $user->password)) {
-            return $this->respondWithError(['Email or password invalid']);
+            return $this->respondWithError('Email or password invalid');
         }
 
         return $this->respondWithSuccess(['message' => 'Logged in successfully', 'user' => $user, 'token' => $this->generateToken($user)]);
@@ -32,7 +33,7 @@ class AuthController extends Controller
     public function signup(SignUp $request)
     {
         $user = User::create($request->except('password') + ['password' => Hash::make($request->password)]);
-        Mail::to($user->email)->later(now()->addSeconds(5), new UserRegistered($user));
+        SendRegistrationMail::dispatch($user);
 
         return $this->respondWithSuccess(['user' => $user, 'token' => $this->generateToken($user)]);
     }
---
+++ app/Jobs/SendRegistrationMail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\UserRegistered;
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendRegistrationMail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    /**
+     * @var User
+     */
+    private $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to($this->user->email)->send(new UserRegistered($this->user));
+    }
+}
